52a8a5bbdf75a427d103fb36055dcea4d0d3d6ad
MrHose <josemiguel.uk@gmail.com>
2021-05-04 11:26:26 +0200

    fixed Random iteration

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,12 +1,10 @@
 import './Random.css'
-
-const random = (Math.floor(Math.random() * 100) + 1)
+ 
 const Random = props => {
-      
     return (
         
         <article>
-           <p>Random Value Between {props.min} and {props.max}: {random} </p>
+           <p>Random Value Between {props.min} and {props.max}: {(Math.floor(Math.random() * props.max) + props.min)} </p>
         </article>
     )
 }
