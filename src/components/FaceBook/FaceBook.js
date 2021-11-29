87ab52e8b53600809346ef48d3d4de49e9018a51
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-11-30 00:24:07 +0100

    Minor refactor in FaceBook component

--- src/components/FaceBook/FaceBook.js
+++ src/components/FaceBook/FaceBook.js
@@ -4,30 +4,27 @@ import './FaceBook.css';
 const FaceBook = () => {
   return (
     <div>
-      <div className="facebook-cards">
-        {profiles.map((profile) => {
-          return (
-            <div className="facebook-container">
-              <img src={profile.img} alt={profile.firstName} />
-              <div>
-                <p>
-                  <span>Name: </span> {profile.firstName}
-                </p>
-                <p>
-                  <span>Last Name: </span> {profile.lastName}
-                </p>
-                <p>
-                  <span>Country: </span> {profile.country}
-                </p>
-                <p>
-                  <span>Type: </span>{' '}
-                  {profile.isStudent ? 'Student' : 'Teacher'}
-                </p>
-              </div>
+      {profiles.map((profile) => {
+        return (
+          <div className="facebook-container">
+            <img src={profile.img} alt={profile.firstName} />
+            <div>
+              <p>
+                <span>Name: </span> {profile.firstName}
+              </p>
+              <p>
+                <span>Last Name: </span> {profile.lastName}
+              </p>
+              <p>
+                <span>Country: </span> {profile.country}
+              </p>
+              <p>
+                <span>Type: </span> {profile.isStudent ? 'Student' : 'Teacher'}
+              </p>
             </div>
-          );
-        })}
-      </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
