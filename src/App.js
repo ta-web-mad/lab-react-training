333ab69ad3b8e10ac31df0a90716d1a48b856470
Adrian Ramirez <ramiire@gmail.com>
2021-11-29 21:06:32 +0100

    iteration 1 complete

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import IdCard from './IdCard';
+// import Greetings from './Greetings';
 
 function App() {
   return (
-    <div className="App">
+
+
+    <div>
 
       {/* TODO Averiguar como poner la fecha */}
-        <IdCard lastName='Doe' firstName="John" gender="male" height='1,78m' birth="1992-07-14" picture="https://randomuser.me/api/portraits/men/44.jpg"/>
-        <IdCard lastName='Delores' firstName="Obrien" gender="female" height='1,72m' birth="1988-05-11" picture="https://randomuser.me/api/portraits/women/44.jpg" />
+      <IdCard lastName='Doe' firstName="John" gender="male" height='1,78m' birth={new Date("1992-07-14").toDateString()} picture="https://randomuser.me/api/portraits/men/44.jpg"/>
+      <IdCard lastName='Delores' firstName="Obrien" gender="female" height='1,72m' birth={new Date("1988-05-11").toDateString()} picture="https://randomuser.me/api/portraits/women/44.jpg" />
+
+
+      
+      {/* <Greetings lang="de" children="Ludwig" />
+      <Greetings lang="fr" children="François" /> */}
+
+
+
+
+
+
 
     </div>
+
+  
+
+
   );
 }
 
---
+++ src/Greetings.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import "./Greetings.css"
+
+
+const Greetings = (props) => {
+
+    return 
+        // <div>
+        //     {props.lang} === 'de'? 
+        //     <p>{props.children}</p>
+
+
+
+        // </div>
+    
+
+
+
+}
+
+export default Greetings
