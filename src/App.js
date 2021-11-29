f6f3c46e047fa4c2fff41780854159a801b883b0
Carlos <carlos_dic1985@hotmail.com>
2021-11-29 23:40:00 +0100

    hecho hasta el 6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import IdCard from './IdCard.js'
 import Greetings from './Greetings.js';
 // import Random from './Random';
 import BoxColor from './BoxColor.js';
+import CreditCard from './CreditCard';
+import Rating from './Rating';
+
 
 
 function App() {
@@ -23,7 +26,7 @@ function App() {
         </div>
         <div>
           <IdCard
-            lastName='Delores '
+            lastName='Delores'
             firstName='Obrien'
             gender='female'
             height={172}
@@ -44,7 +47,7 @@ function App() {
       <section className="it3">
         {/* <Random min={1} max={6} />
         <Random min={1} max={100} />
- */}
+        */}
       </section>
 
       {/* INTERACIÓN 4 */}
@@ -52,7 +55,59 @@ function App() {
         <BoxColor r={255} g={0} b={0} />
         <BoxColor r={128} g={255} b={0} />
       </section>
+
+      {/* INTERACIÓN 5 */}
+      <section classname="it5">
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white"
+        />
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222"
+        />
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white"
+        />
+      </section>
+
+      {/* INTERACCIÓN 6 */}
+      <section classname="int6">
+        <Rating>0</Rating>
+        <Rating>1.49</Rating>
+        <Rating>1.5</Rating>
+        <Rating>3</Rating>
+        <Rating>4</Rating>
+        <Rating>5</Rating>
+
+
+
+
+      </section>
+
+
+
     </div>
+
   );
 }
 
